c7ee0301fd1dd7a25fc5244d644f45bfa8e0f47c
milkmidi <milkmidi@gmail.com>
2022-07-24 21:21:53 +0800

    feat: adjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,12 @@ module.exports = {
     require('daisyui'),
     plugin(({ addUtilities, matchUtilities, addComponents, addVariant, e, theme }) => {
       addVariant('child', ' & > *');
+      addVariant('data-active', ({ modifySelectors, separator }) => {
+        modifySelectors(
+          ({ className }) =>
+            `.${e(`data-active${separator}${className}`)}[data-active="true"]`,
+        );
+      });
     }),
   ],
 };
